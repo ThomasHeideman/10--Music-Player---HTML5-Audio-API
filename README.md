9a19884363b8dd07b3dfb2ec6132ce2799c7f220
ThomasHeideman <47209974+ThomasHeideman@users.noreply.github.com>
2024-06-21 16:43:02 +0200

    finished project

--- README.md
+++ README.md
@@ -1,16 +1,13 @@
-# Project Name
+# Music Player
 
-    There are only two kinds of programming languages: those people always bitch about and those nobody uses. (Bjarne Stroustrup)
+    Create  UI to play music stored in the "music folder" using the HTML5 audio API
 
 # Project Specifications
 
     HTML/CSS:
-        ✅ The best method for accelerating a computer is the one that boosts it by 9.8 m/s2. (Anonymous)
-
+        ✅ Create UI for music player including spinning image and song detail popup
 
     JS:
-        ✅ Controlling complexity is the essence of computer programming. (Brian Kernigan)
-        ✅ Good code is its own best documentation. (Steve McConnell)
-        ✅ To err is human, but to really foul things up you need a computer. (Paul Ehrlich)
-        ✅ If Java had true garbage collection, most programs would delete themselves upon execution. (Robert Sewell)
-        ✅ 19 Jan 2038 at 3:14:07 AM (End of the word according to Unix–2^32 seconds after January 1, 1970)
+        ✅ Add play and pause functionality
+        ✅ Switch songs
+        ✅ Progress bar
--- script.js
+++ script.js
@@ -1,1 +1,107 @@
-'use strict';
+"use strict";
+const musicContainer = document.getElementById("music-container");
+const musicInfo = document.getElementById("music-info");
+
+const playBtn = document.getElementById("play");
+const prevBtn = document.getElementById("prev");
+const nextBtn = document.getElementById("next");
+
+const audio = document.getElementById("audio");
+const progressContainer = document.getElementById("progress-container");
+const progress = document.getElementById("progress");
+const title = document.getElementById("title");
+const cover = document.getElementById("cover");
+
+// Song Titles
+const songs = ["hey", "summer", "ukelele"];
+
+// Keep track of song
+
+let songIndex = 0;
+
+// Initially load song details into DOM
+loadSong(songs[songIndex]);
+
+// Update song details
+function loadSong(song) {
+  title.innerText = song;
+  audio.src = `music/${song}.mp3`;
+  cover.src = `src/${song}.jpg`;
+}
+
+// Play song
+function playSong() {
+  musicContainer.classList.add("play");
+  playBtn.querySelector("i.fa-solid").classList.remove("fa-play");
+  playBtn.querySelector("i.fa-solid").classList.add("fa-pause");
+  audio.play();
+}
+// Pause song
+function pauseSong() {
+  musicContainer.classList.remove("play");
+  playBtn.querySelector("i.fa-solid").classList.remove("fa-pause");
+  playBtn.querySelector("i.fa-solid").classList.add("fa-play");
+  audio.pause();
+}
+// Change song
+function prevSong() {
+  console.log("click");
+  songIndex--;
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+  loadSong(songs[songIndex]);
+
+  musicContainer.classList.contains("play") ? playSong() : showInfo();
+}
+
+function nextSong() {
+  songIndex++;
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+  loadSong(songs[songIndex]);
+  musicContainer.classList.contains("play") ? playSong() : showInfo();
+}
+
+// Set progress bar
+function setProgress(e) {
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const duration = audio.duration;
+
+  audio.currentTime = (clickX / width) * duration;
+}
+
+// Update Progress
+function updateProgress(e) {
+  const { duration, currentTime } = e.srcElement;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
+}
+
+// show info
+function showInfo() {
+  musicInfo.classList.add("show");
+  setTimeout(() => {
+    musicInfo.classList.remove("show");
+  }, 750);
+}
+
+// event listeners
+playBtn.addEventListener("click", () => {
+  const isPlaying = musicContainer.classList.contains("play");
+  isPlaying ? pauseSong() : playSong();
+});
+// Change song
+prevBtn.addEventListener("click", prevSong);
+nextBtn.addEventListener("click", nextSong);
+
+// Time/song update
+
+audio.addEventListener("timeupdate", updateProgress);
+// Click on progress bar
+progressContainer.addEventListener("click", setProgress);
+
+// Song ends
+audio.addEventListener("ended", nextSong);
